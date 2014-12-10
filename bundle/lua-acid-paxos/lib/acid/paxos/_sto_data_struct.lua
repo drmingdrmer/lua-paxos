0ebd62c06118cf81587f59a2d9a5d12a2f27eb4f
drdr xp <drdr.xp@gmail.com>
2014-12-10 14:50:00 +0800

    update sample data type

--- bundle/lua-acid-paxos/lib/acid/paxos/_sto_data_struct.lua
+++ bundle/lua-acid-paxos/lib/acid/paxos/_sto_data_struct.lua
@@ -1,9 +1,12 @@
+local member_id = { cluster_id="123.dx.GZ", ident="<partition_id>" }
 local committed = {
     ver = 1,
     val = {
         view = {
-            { a=1, b=1, c=1 },
-            { b=1, c=1, d=1 },
+            {
+                ['<ident>']={index=0, ip="127.0.0.1"},
+                ['<ident>']={index=1, ip="127.0.0.1"},
+            },
         },
         leader = { ident="id", __lease=10 },
         action = {
