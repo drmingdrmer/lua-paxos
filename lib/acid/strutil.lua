a7e50bb74cca4e4dd142c1290f3857d767a6070d
drdr xp <drdr.xp@gmail.com>
2017-02-26 18:32:34 +0800

    strutil and test

--- lib/acid/strutil.lua
+++ lib/acid/strutil.lua
@@ -1,14 +1,24 @@
 local _M = { _VERSION = "0.1" }
 
-local tableutil = require( "acid.tableutil" )
+local json = require( "cjson" )
 
-function  _M.split( str, pat )
+-- TODO test plain
+function  _M.split( str, pat, plain )
 
     local t = {}  -- NOTE: use {n = 0} in Lua-5.0
+
+    if pat == '' then
+        for i = 1, #str do
+            table.insert(t, str:sub(i, i))
+        end
+        t[1] = t[1] or ''
+        return t
+    end
+
     local last_end, s, e = 1, 1, 0
 
     while s do
-        s, e = string.find( str, pat, last_end )
+        s, e = string.find( str, pat, last_end, plain )
         if s then
             table.insert( t, str:sub( last_end, s-1 ) )
             last_end = e + 1
@@ -21,7 +31,7 @@ end
 
 function  _M.strip( s, ptn )
 
-    if ptn == nil then
+    if ptn == nil or ptn == "" then
         ptn = "%s"
     end
 
@@ -40,38 +50,48 @@ function  _M.endswith( s, suf )
     return s:sub( -suf:len(), -1 ) == suf
 end
 
-function _M.to_str( ... )
+function _M.to_str(...)
 
     local argsv = {...}
     local v
 
-    for i=1, base.select('#', ...) do
+    for i=1, select('#', ...) do
         v = argsv[i]
-        if base.type(v) == 'table' then
-            argsv[i] = tableutil.repr(v)
+        if type(v) == 'table' then
+            argsv[i] = json.encode(v)
         else
-            argsv[i] = base.tostring(v)
+            argsv[i] = tostring(v)
         end
     end
 
-    return table.concat( argsv )
+    return table.concat(argsv)
 end
 
-function _M.ljust( str, n, ch )
+-- TODO test
+function _M._placeholder(val, pholder)
+    pholder = pholder or '-'
 
-    return str .. string.rep( ch or ' ', n - string.len( str ) )
+    if val == '' or val == nil then
+        return pholder
+    else
+        return val
+    end
 end
 
-function _M.rjust( str, n, ch )
 
-    return string.rep( ch or ' ', n - string.len( str ) ) .. str
+function _M.ljust(str, n, ch)
+    return str .. string.rep(ch or ' ', n - string.len(str))
+end
+
+function _M.rjust(str, n, ch)
+    return string.rep(ch or ' ', n - string.len(str)) .. str
 end
 
 function _M.replace(s, src, dst)
-    return table.concat(split(s, src), dst)
+    return table.concat(_M.split(s, src), dst)
 end
 
-function _M.parse_fnmatch_char(a1)
+local function _parse_fnmatch_char(a1)
     if a1 == "*" then
         return ".*"
     elseif a1 == "?" then
@@ -86,23 +106,20 @@ end
 function _M.fnmatch(s, ptn)
     local p = ptn
     local p = p:gsub('([\\]*)(.)', function(a0, a1)
-        if bs == "" then
-            return _M.parse_fnmatch_char(a1)
+        local l = #a0
+        if l % 2 == 0 then
+            return string.rep('[\\]', l/2).._parse_fnmatch_char(a1)
         else
-            local l = #a0
-            if l % 2 == 0 then
-                return string.rep('[\\]', l/2) .. _M.parse_fnmatch_char(a1)
-            else
-                return string.rep('[\\]', (l-1)/2) .. '[' .. a1 .. ']'
-            end
+            return string.rep('[\\]', (l-1)/2)..'['.. a1 ..']'
         end
     end)
+
     return s:match(p) == s
 end
 
 function _M.fromhex(str)
     return (str:gsub('..', function (cc)
-        return string.char(base.tonumber(cc, 16))
+        return string.char(tonumber(cc, 16))
     end))
 end
 
--- lib/test_strutil.lua
+++ lib/test_strutil.lua
@@ -1,8 +1,10 @@
-strutil = require( "acid.strutil" )
+local strutil = require("acid.strutil")
 
-local str ='/v1/get/video.vic.sina.com.cn%2fmt788%2f9e%2f1a%2f81403.jpg/%7b%22xACL%22%3a%20%7b%22GRPS000000ANONYMOUSE%22%3a%20%5b%22read%22%5d, %20%22SINA00000000000SALES%22%3a%20%5b%22read%22, %20%22write%22, %20%22read_acp%22, %20%22write_acp%22%5d%7d, %20%22Info%22%3a%20null, %20%22Type%22%3a%20%22image%5c%2fjpeg%22, %20%22ver%22%3a%201042410872, %20%22Get-Location%22%3a%20%5b%7b%22CheckNumber%22%3a%201042410872, %20%22GroupID%22%3a%20341476, %20%22Partitions%22%3a%20%5b%7b%22IPs%22%3a%20%5b%2258.63.236.89%22, %20%2210.71.5.89%22%5d, %20%22PartitionID%22%3a%20%22185c3e5700014004975f90b11c13fc5e%22, %20%22IDC%22%3a%20%22.dx.GZ%22%7d, %20%7b%22IPs%22%3a%20%5b%2258.63.236.184%22, %20%2210.71.5.184%22%5d, %20%22PartitionID%22%3a%20%225a56155500014009aaa590b11c148e88%22, %20%22IDC%22%3a%20%22.dx.GZ%22%7d, %20%7b%22IPs%22%3a%20%5b%2260.28.228.36%22, %20%22172.16.228.36%22%5d, %20%22PartitionID%22%3a%20%22a41d4006000140029595d4ae52b17fe1%22, %20%22IDC%22%3a%20%22.wt.TJ%22%7d, %20%7b%22IPs%22%3a%20%5b%22111.161.78.59%22, %20%22172.16.48.59%22%5d, %20%22PartitionID%22%3a%20%22d7fee54d00014006b58090b11c145321%22, %20%22IDC%22%3a%20%22.wt.TJ%22%7d%5d%7d%5d, %20%22Info-Int%22%3a%200, %20%22ts%22%3a%201356544141, %20%22ACL%22%3a%20%7b%22SINA00000000000SALES%22%3a%20%5b%22read%22, %20%22write%22, %20%22read_acp%22, %20%22write_acp%22%5d%7d, %20%22ETag2%22%3a%20%2235b4ec0bfd826ea609054ccca4976e4fc77f3a8b%22, %20%22ETag%22%3a%20%22e14526f8858e2e0f898e72f141f108e4%22, %20%22Key%22%3a%20%22mt788%5c%2f9e%5c%2f1a%5c%2f81403.jpg%22, %20%22Owner%22%3a%20%22SINA00000000000SALES%22, %20%22Origo%22%3a%20%220000000000000000000090b11c09b4d9%22, %20%22GroupClassID%22%3a%206, %20%22File-Meta%22%3a%20%7b%22Content-Type%22%3a%20%22image%5c%2fjpeg%22%7d, %20%22Size%22%3a%2095964%7d?n=1&r=1&w=1&expire=60&ver_key=ts'
 
 function test_split(t)
+
+    local str ='/v1/get/video.vic.sina.com.cn%2fmt788%2f9e%2f1a%2f81403.jpg/%7b%22xACL%22%3a%20%7b%22GRPS000000ANONYMOUSE%22%3a%20%5b%22read%22%5d, %20%22SINA00000000000SALES%22%3a%20%5b%22read%22, %20%22write%22, %20%22read_acp%22, %20%22write_acp%22%5d%7d, %20%22Info%22%3a%20null, %20%22Type%22%3a%20%22image%5c%2fjpeg%22, %20%22ver%22%3a%201042410872, %20%22Get-Location%22%3a%20%5b%7b%22CheckNumber%22%3a%201042410872, %20%22GroupID%22%3a%20341476, %20%22Partitions%22%3a%20%5b%7b%22IPs%22%3a%20%5b%2258.63.236.89%22, %20%2210.71.5.89%22%5d, %20%22PartitionID%22%3a%20%22185c3e5700014004975f90b11c13fc5e%22, %20%22IDC%22%3a%20%22.dx.GZ%22%7d, %20%7b%22IPs%22%3a%20%5b%2258.63.236.184%22, %20%2210.71.5.184%22%5d, %20%22PartitionID%22%3a%20%225a56155500014009aaa590b11c148e88%22, %20%22IDC%22%3a%20%22.dx.GZ%22%7d, %20%7b%22IPs%22%3a%20%5b%2260.28.228.36%22, %20%22172.16.228.36%22%5d, %20%22PartitionID%22%3a%20%22a41d4006000140029595d4ae52b17fe1%22, %20%22IDC%22%3a%20%22.wt.TJ%22%7d, %20%7b%22IPs%22%3a%20%5b%22111.161.78.59%22, %20%22172.16.48.59%22%5d, %20%22PartitionID%22%3a%20%22d7fee54d00014006b58090b11c145321%22, %20%22IDC%22%3a%20%22.wt.TJ%22%7d%5d%7d%5d, %20%22Info-Int%22%3a%200, %20%22ts%22%3a%201356544141, %20%22ACL%22%3a%20%7b%22SINA00000000000SALES%22%3a%20%5b%22read%22, %20%22write%22, %20%22read_acp%22, %20%22write_acp%22%5d%7d, %20%22ETag2%22%3a%20%2235b4ec0bfd826ea609054ccca4976e4fc77f3a8b%22, %20%22ETag%22%3a%20%22e14526f8858e2e0f898e72f141f108e4%22, %20%22Key%22%3a%20%22mt788%5c%2f9e%5c%2f1a%5c%2f81403.jpg%22, %20%22Owner%22%3a%20%22SINA00000000000SALES%22, %20%22Origo%22%3a%20%220000000000000000000090b11c09b4d9%22, %20%22GroupClassID%22%3a%206, %20%22File-Meta%22%3a%20%7b%22Content-Type%22%3a%20%22image%5c%2fjpeg%22%7d, %20%22Size%22%3a%2095964%7d?n=1&r=1&w=1&expire=60&ver_key=ts'
+
     t:eqdict( { '' }, strutil.split( '', '/' ), 'empty string' )
     t:eqdict( { '', '' }, strutil.split( '/', '/' ), 'single pattern' )
     t:eqdict( { '', '', '' }, strutil.split( '//', '/' ), 'dual pattern' )
@@ -41,7 +43,6 @@ function test_endswith(t)
     t:eq(true, s( 'ab', 'ab' ) )
     t:eq(false, s( 'ab', 'bc' ) )
 end
-
 
 function test_rjust(t)
     local f = strutil.rjust
@@ -121,3 +122,22 @@ function test_fnmatch(t)
     t_match('\\?', '\\\\?', true)
     t_match('\\a', '\\\\?', true)
 end
+
+
+function bench_split()
+
+    local str ='/v1/get/video.vic.sina.com.cn%2fmt788%2f9e%2f1a%2f81403.jpg/%7b%22xACL%22%3a%20%7b%22GRPS000000ANONYMOUSE%22%3a%20%5b%22read%22%5d, %20%22SINA00000000000SALES%22%3a%20%5b%22read%22, %20%22write%22, %20%22read_acp%22, %20%22write_acp%22%5d%7d, %20%22Info%22%3a%20null, %20%22Type%22%3a%20%22image%5c%2fjpeg%22, %20%22ver%22%3a%201042410872, %20%22Get-Location%22%3a%20%5b%7b%22CheckNumber%22%3a%201042410872, %20%22GroupID%22%3a%20341476, %20%22Partitions%22%3a%20%5b%7b%22IPs%22%3a%20%5b%2258.63.236.89%22, %20%2210.71.5.89%22%5d, %20%22PartitionID%22%3a%20%22185c3e5700014004975f90b11c13fc5e%22, %20%22IDC%22%3a%20%22.dx.GZ%22%7d, %20%7b%22IPs%22%3a%20%5b%2258.63.236.184%22, %20%2210.71.5.184%22%5d, %20%22PartitionID%22%3a%20%225a56155500014009aaa590b11c148e88%22, %20%22IDC%22%3a%20%22.dx.GZ%22%7d, %20%7b%22IPs%22%3a%20%5b%2260.28.228.36%22, %20%22172.16.228.36%22%5d, %20%22PartitionID%22%3a%20%22a41d4006000140029595d4ae52b17fe1%22, %20%22IDC%22%3a%20%22.wt.TJ%22%7d, %20%7b%22IPs%22%3a%20%5b%22111.161.78.59%22, %20%22172.16.48.59%22%5d, %20%22PartitionID%22%3a%20%22d7fee54d00014006b58090b11c145321%22, %20%22IDC%22%3a%20%22.wt.TJ%22%7d%5d%7d%5d, %20%22Info-Int%22%3a%200, %20%22ts%22%3a%201356544141, %20%22ACL%22%3a%20%7b%22SINA00000000000SALES%22%3a%20%5b%22read%22, %20%22write%22, %20%22read_acp%22, %20%22write_acp%22%5d%7d, %20%22ETag2%22%3a%20%2235b4ec0bfd826ea609054ccca4976e4fc77f3a8b%22, %20%22ETag%22%3a%20%22e14526f8858e2e0f898e72f141f108e4%22, %20%22Key%22%3a%20%22mt788%5c%2f9e%5c%2f1a%5c%2f81403.jpg%22, %20%22Owner%22%3a%20%22SINA00000000000SALES%22, %20%22Origo%22%3a%20%220000000000000000000090b11c09b4d9%22, %20%22GroupClassID%22%3a%206, %20%22File-Meta%22%3a%20%7b%22Content-Type%22%3a%20%22image%5c%2fjpeg%22%7d, %20%22Size%22%3a%2095964%7d?n=1&r=1&w=1&expire=60&ver_key=ts'
+
+    local xx = strutil.split( str, '/' )
+    for i, v in ipairs(xx) do
+        print( v )
+    end
+
+    local i = 1024 * 1024
+    while i > 0 do
+        xx = strutil.split( str, '/')
+        i = i - 1
+    end
+
+end
+
